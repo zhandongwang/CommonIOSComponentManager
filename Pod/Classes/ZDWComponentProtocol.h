994bc9f86c5e93f45ad0b5554b507d4b5e6d8281
fengli <fengli@2dfire.com>
2016-11-23 15:33:36 +0800

    添加注册方法

---
+++ Pod/Classes/ZDWComponentProtocol.h
@@ -0,0 +1,13 @@
+//
+//  ZDWComponentManager.h
+//  Pods
+//
+//  Created by 王战东 on 16/11/23.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZDWComponentProtocol : NSObject
+
+@end
---
+++ Pod/Classes/ZDWComponentProtocol.m
@@ -0,0 +1,13 @@
+//
+//  ZDWComponentManager.m
+//  Pods
+//
+//  Created by 王战东 on 16/11/23.
+//
+//
+
+#import "ZDWComponentProtocol.h"
+
+@implementation ZDWComponentProtocol
+
+@end
